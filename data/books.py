4fb7308887230a5197915c825c184df904e27484
BISorig <bikilya5@gmail.com>
2023-04-13 21:47:19 +0300

    add bd

---
+++ data/books.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Book(SqlAlchemyBase):
+    __tablename__ = 'books'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    genre_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('genres.id'))
+    author_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('authors.id'))
+    genre = orm.relationship('Genre')
+    author = orm.relationship('Author')
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from data import db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -10,4 +12,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/books.db")
     app.run(port=5000, host='127.0.0.1')
