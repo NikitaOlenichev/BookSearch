becd595a7ba4b9bdaaa2dba8c43e42d4943e8096
Nikita Olenichev <2020nikol@mail.ru>
2023-04-13 16:14:21 +0300

    add reg and avtor

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from forms.login_form import LoginForm
+from forms.register_form import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+        return redirect('/')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form, message='Пароли не совпадают')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
