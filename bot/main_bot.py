8c1625d0640baa72b56b43f159b204f619e4df0e
Nikita Olenichev <2020nikol@mail.ru>
2023-04-20 15:32:47 +0300

    add bot frame

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -8,6 +8,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+TOKEN = '6192417970:AAG5Jb2pJVSGQvlW8LGVOGOZCI82D2qeFSU'
+
 
 async def start(update, context):
     await update.message.reply_text('Привет!!! Я - бот от КнигаПоиска!\n'
@@ -18,8 +20,23 @@ async def help_command(update, context):
     await update.message.reply_text('Помощь уже в пути!')
 
 
+async def search(update, context, book):
+    pass
+
+
+async def top_read(update, context):
+    pass
+
+
+async def genres_book(update, context, genre):
+    pass
+
+
+async def authors_book(update, context, author):
+    pass
+
+
 def main():
-    TOKEN = '6192417970:AAG5Jb2pJVSGQvlW8LGVOGOZCI82D2qeFSU'
     application = Application.builder().token(TOKEN).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('help', help_command))
