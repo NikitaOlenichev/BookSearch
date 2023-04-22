006f1b7bb6e683c0e56d54180207f96602107752
BISorig <bikilya5@gmail.com>
2023-04-22 22:37:24 +0300

    update search

--- add.py
+++ add.py
@@ -12,13 +12,13 @@ ats = []
 db_session.global_init("db/books.db")
 db_sess = db_session.create_session()
 
-for i in range(90, 121):
+for i in range(10, 300):
     res = requests.get(f'https://api.fantlab.ru/work/{i}/extended').json()
     try:
         title = res['work_name']
     except Exception:
         title = '-'
-    if title == '-':
+    if title == '-' or not title:
         continue
     if not db_sess.query(Book).filter(Book.title == title).first():
         try:
@@ -51,10 +51,10 @@ for i in range(90, 121):
         db_sess.commit()
         db_sess.add(Book(
             title=title,
-            info=db_sess.query(Info).filter(Info.info == info).first().id,
+            info_id=db_sess.query(Info).filter(Info.info == info).first().id,
             genre_id=db_sess.query(Genre).filter(Genre.title == genre).first().id,
             author_id=db_sess.query(Author).filter(Author.name == author).first().id,
-            image=db_sess.query(Image).filter(Image.link == image).first().id
+            image_id=db_sess.query(Image).filter(Image.link == image).first().id
         ))
         db_sess.commit()
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data import db_session
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from forms.login_form import LoginForm
 from forms.register_form import RegisterForm
 from flask_login import LoginManager, login_user, logout_user, login_required
@@ -44,6 +44,7 @@ def login():
     form = LoginForm()
     print(form.username)
     if form.validate_on_submit():
+        print(2)
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.username.data).first()
         if user and user.check_password(form.password.data):
@@ -56,7 +57,7 @@ def login():
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
@@ -83,21 +84,46 @@ def reqister():
 @app.route('/search/<int:page>', methods=['GET', 'POST'])
 def search(page):
     db_sess = db_session.create_session()
-    sp = db_sess.query(Book).filter(Book.id < 11).all()
+    form = Search()
+    sp = db_sess.query(Book)
+    if form.validate_on_submit():
+        fltr = f'?title={form.title.data}&author={form.author.data}&genre={form.genre.data}'
+        return redirect(f'/search/1{fltr}')
     sp2 = []
-    for book in sp:
+    author_fltr = request.args.get('author')
+    genre_fltr = request.args.get('genre')
+    title_fltr = request.args.get('title')
+    print(title_fltr)
+    if title_fltr:
+        sp = sp.filter(Book.title == title_fltr)
+        form.title.data = title_fltr
+    if genre_fltr:
+        sp = sp.filter(Book.genre == db_sess.query(Genre).filter(Genre.title == genre_fltr).first())
+        form.genre.data = genre_fltr
+    if author_fltr:
+        sp = sp.filter(Book.author == db_sess.query(Author).filter(Author.name == author_fltr).first())
+        form.author.data = author_fltr
+    sp = sp.all()
+    sp.sort(key=lambda x: x.title)
+    for book in sp[10 * (page - 1): 10 * page]:
         author = db_sess.query(Author).filter(Author.id == book.author_id).first()
-        image = db_sess.query(Image).filter(Image.id == book.image).first()
-        info = db_sess.query(Info).filter(Info.id == book.info).first()
+        image = db_sess.query(Image).filter(Image.id == book.image_id).first()
+        info = db_sess.query(Info).filter(Info.id == book.info_id).first()
         genre = db_sess.query(Genre).filter(Genre.id == book.genre_id).first()
         sp2.append([image.link,
                     book.title,
                     author.name,
                     genre.title,
                     info.info])
-    print(sp2)
-    form = Search()
-    return render_template('search.html', title='Поиск', form=form, sp=sp2)
+    ln = (len(sp) - 1) // 10 + 1
+    pg_f = max(page - 4, 1)
+    pg_l = min(page + 4, ln)
+    if page < 5:
+        pg_l = min(9, ln)
+    if page > ln - 4:
+        pg_f = max(1, ln - 8)
+    return render_template('search.html', title='Поиск', form=form, sp=sp2, page=page, link=f'/search/', ln=len(sp), msg='',
+                           author_fltr=author_fltr if author_fltr else '', genre_fltr=genre_fltr if genre_fltr else '', ttl_fltr=title_fltr if title_fltr else '', pg_l=pg_l, pg_f=pg_f)
 
 
 if __name__ == '__main__':
